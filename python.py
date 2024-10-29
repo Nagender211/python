9569b15537f63906dae87694f9cab4c9b5914ddc
Nagender211 <dnagender2019@gmail.com>
2024-10-29 23:05:54 +0530

    python

--- python.py
+++ python.py
@@ -15,10 +15,147 @@ print( 193856 - 596 )
 print(37*67*391)
 
 #print input 
-a=input()
-print(a)
+# a=input()
+# print(a)
 
 #print multiple values
+# a=input()
+# print(a)
+# print("###")
+
+# print a string with multiple line
+# a=input()
+# b=input()
+# print(a)
+# print(b)
+
+# read two line of the code and print 
+# a=input()
+# b=input()
+# print(b)
+
+# print the revereser the code us the 
+# a= input()
+# b= input()
+# print(b)
+# print(a)
+
+
+# print the space the 
+# a=input()
+# b=input()
+# print(a+ ' ' + b)
+
+# print the Name and the age
+# a=input()
+# b=int(input())
+# print(a, "age is" ,b)
+
+# print word by 3 times
+# a=input()
+# print((a+" ")*3)
+
+
+# print * in square
+# print(("*"*2)+("\n")+("*"*2))
+
+
+# print the star with space
+# print(("*"+" ")*2+("\n")+("*"+ " ")*2)
+
+# print the triangle
+# print("*"+("\n")+("*")*2)
+
+# print the triangle
+# print("*"+("\n")+("*"+" ")*2)
+
+# print the frmate string
+# a=input()
+# print(("*"+"")*3+" "+ a +" "+ ("*"+ " ")*3)
+
+# read the first charetcter
+# a=input()
+# print(a[0])
+
+# read the third charetcter
+# a=input()
+# print(a[2])
+
+
+# first and last char
+# a=int(input())
+# b=str(a)
+# print(b[0])
+# print(b[-1])
+
+# reverse the number
+# a=int(input())
+# b=str(a)
+# print(b[-1]+b[0])
+
+# length of the string
+# a=input()
+# print(len(a))
+
+# subtract 1 from the length
+# a=(input())
+# print(len(a)-1)
+
+# last chartector 
+# a=input()
+# print(a[-1])
+
+# expect the first and last word print all
+# a=input()
+# print(a[1:-1])
+
+# print first letter and remaing are star
+# a=input()
+# print(a[0]+"*"*(len(a)-2)+a[-1])
+
+# print the half of the length 
+# a=input()
+# print(len(a)/2)
+
+# # rectangle print two star
+# print(("*"+" ")*2+ ("\n")+("*"+" ")*2+("\n")+("*"+" ")*2)
+
+# # rectangle print three star
+# print(("*"+" ")*3+ ("\n")+("*"+" ")*3+("\n")+("*"+" ")*3)
+
+
+# # rectangle print three star
+# print(("*"+" ")*1+ ("\n")+("*"+" ")*2+("\n")+("*"+" ")*3)
+
+# len star-2
+# a=input()
+# print("*"*len(a) + " " + a + " " + "*"*len(a))
+
+# two words
+# w1=input()
+# w2=input()
+# end=len(w2)-1
+# print(end)
+
+# revers the words
+# a=input()
+# b=input()
+# print(b)
+# print("###")
+# print(a)
+
+# first and last line 
+# a=input()
+# print(a[0])
+# print(a[-1])
+
+# first lettrs of the word
+# a=input()
+# b=input()
+# c=input()
+# print(a[0]+b[0]+c[0])
+
+
+# first and last only remaing *
 a=input()
-print(a)
-print("###")
+print(a[0]+"*"*(len(a)-2)+a[-1])
